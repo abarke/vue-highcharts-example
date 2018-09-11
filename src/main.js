3f9e7cef1a550dad7f2d33c2040936c04eac99bc
Inversion <inversion@zeitgeistmovement.de>
2018-09-11 23:24:53 +0200

    New: HighCharts Example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import HighchartsVue from 'highcharts-vue'
+
+Vue.use(HighchartsVue)
 
 Vue.config.productionTip = false
 
